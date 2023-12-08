39d9699a787a2474143d17516d0917f45eb2821a
Benjamin Menetrier <benjamin.menetrier@met.no>
2023-12-08 10:24:47 +0100

    Working now

--- podcast.py
+++ podcast.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
-import os
+import argparse
+import calendar
+import datetime
 import json
-from urllib.request import urlretrieve
 from rss_parser import Parser
 from requests import get
+import os
 import pathlib
+import re
 import subprocess
-import datetime
-import calendar
+import unicodedata
+from urllib.request import urlretrieve
+
+# Parser
+parser = argparse.ArgumentParser()
+#parser.add_argument("podcast_dir", help="Podcast directory", required=True)
+parser.add_argument("--initialize", help="Initialization mode", action="store_true")
+parser.add_argument("--dryrun", help="Dry run (no download)", action="store_true")
+
+# Parse and print arguments
+args = parser.parse_args()
+print("Parameters:")
+for arg in vars(args):
+    if not arg is None:
+        print(" - " + arg + ": " + str(getattr(args, arg)))
 
 # Podcast files folder
-podcast_dir=os.path.expanduser('~') + "/Music/PodcastTest"
+podcast_dir=os.path.expanduser("~") + "/Music/PodcastTest" # => should be in parser
 
 # Script directory
 base_dir=os.path.dirname(os.path.realpath(__file__))
@@ -21,14 +37,15 @@ os.makedirs(podcast_dir, exist_ok=True)
 os.makedirs(os.path.join(base_dir, "rss"), exist_ok=True)
 
 # Load data base or initialize
-if os.path.exists('database.json'):
+if os.path.exists("database.json"):
   database = json.load(open("database.json"))
-  with open('database.json.bak', 'w', encoding ='utf8') as json_file: 
-  	json.dump(database, json_file, ensure_ascii=True, indent=2) 
+  with open("database.json.bak", "w", encoding ="utf8") as json_file: 
+    json.dump(database, json_file, ensure_ascii=True, indent=2) 
 else:
   database = {}
 
 # Fill rss base
+print("Processing RSS feeds")
 rssbase = {}
 for line in open(os.path.join(base_dir, "serverlist")):
   li=line.strip()
@@ -40,21 +57,24 @@ for line in open(os.path.join(base_dir, "serverlist")):
     
     response = get(url)
     rss = Parser.parse(response.text)
-    print(rss)
-    exit()
+
     for item in rss.channel.items:
-      url = item.enclosure.attributes['url']    
+      url = item.enclosure.attributes["url"]    
       itemData = {}
       itemData["title"] = item.title.content.replace("’", "'")
-      filename = "".join(x for x in itemData["title"] if x.isalnum())
+      filename = itemData["title"]
+      filename = unicodedata.normalize("NFKD", filename).encode("ascii", "ignore").decode("ascii")
+      filename = re.sub(r"[^\w\s-]", "", filename.lower())
+      filename = re.sub(r"[-\s]+", "-", filename).strip("-_")
       itemData["artist"] = artist
       itemData["album"] = album
-      itemData["url"] = item.enclosure.attributes['url']
+      itemData["url"] = item.enclosure.attributes["url"]
       dateElements = item.pub_date.content.split(" ")
       day = int(dateElements[1])
       month = list(calendar.month_abbr).index(dateElements[2])
       year = int(dateElements[3])
       itemData["date"] = str(datetime.date(year, month, day))
+      filename = itemData["date"] + "_" + filename + ".mp3"
       rssbase[filename] = itemData
 
 # Fill files base
@@ -66,47 +86,61 @@ for item in pathlib.Path(podcast_dir).rglob("*.mp3"):
   filebase[filename] = str(item)
 
 # Full list
-fulllist = []
+fullbase = {}
 for item in database:
-  fulllist.append(item)
+  fullbase[item] = database[item]
 for item in rssbase:
-  if not item in fulllist:
-    fulllist.append(item)
+  if not item in fullbase:
+    fullbase[item] = rssbase[item]
 for item in filebase:
-  if not item in fulllist:
-    fulllist.append(item)  
+  if not item in fullbase:
+    fullbase = filebase[item]
+
+# Initialization case:
+if args.initialize:
+  with open("download.json", "w", encoding ="utf8") as json_file: 
+    json.dump(fullbase, json_file, ensure_ascii=True, indent=2)
+  dummy = input("Update download.json manually and type any key when it is done...")
+  downloadbase = json.load(open("download.json"))
 
 # Loop over full list
-for item in fulllist:
-  print("File " + item)
+for item in fullbase:
   if (item in database) and (not item in filebase) and (not item in rssbase):
     # File already listened and not in RSS anymore: remove from database
     print("Removing " + database[item] + " from database")
     database.pop(item)
-    with open('database.json', 'w', encoding ='utf8') as json_file: 
-    	json.dump(database, json_file, ensure_ascii=True, indent=2) 
+    with open("database.json", "w", encoding ="utf8") as json_file: 
+      json.dump(database, json_file, ensure_ascii=True, indent=2) 
   elif (not item in database) and (item in filebase):
     # File present but not in database
     print("Resetting " + item + " in database")
     database[item] = filebase[item]
-    with open('database.json', 'w', encoding ='utf8') as json_file: 
-    	json.dump(database, json_file, ensure_ascii=True, indent=2) 
+    with open("database.json", "w", encoding ="utf8") as json_file: 
+      json.dump(database, json_file, ensure_ascii=True, indent=2) 
   elif (not item in database) and (not item in filebase) and (item in rssbase):
-    # Download new file, change metadata and add to database
+    # Download new file if neeeded
     url = rssbase[item]["url"]
-    filepath = os.path.join(podcast_dir, rssbase[item]["artist"], rssbase[item]["album"], item)
-    name = input("Download " + rssbase[item]["artist"] + " - " + rssbase[item]["album"] + " - " + rssbase[item]["title"] + " ? [y] ")
-    if name == "y":
+    filepath = os.path.join(podcast_dir, rssbase[item]["artist"], rssbase[item]["album"], item)    
+    toDownload = not args.dryrun
+    if args.initialize and (not item in downloadbase):
+      toDownload = False
+
+    if toDownload:
+      # Download
       print("Downloading " + filepath)
       os.makedirs(os.path.dirname(filepath), exist_ok=True)
-      urlretrieve(url, filepath)     
+      urlretrieve(url, filepath)
+
+      # Update mp3 metadata
       subprocess.run(["id3v2", "--artist", rssbase[item]["artist"], filepath])
       subprocess.run(["id3v2", "--album", rssbase[item]["album"], filepath])
       subprocess.run(["id3v2", "--song", rssbase[item]["title"], filepath])
       subprocess.run(["id3v2", "--year", rssbase[item]["date"], filepath])
       subprocess.run(["id3v2", "--genre", "Podcast", filepath])
+
+    # Add to database
     database[item] = filepath
-    with open('database.json', 'w', encoding ='utf8') as json_file: 
-    	json.dump(database, json_file, ensure_ascii=True, indent=2) 
+    with open("database.json", "w", encoding ="utf8") as json_file: 
+      json.dump(database, json_file, ensure_ascii=True, indent=2) 
   elif (not item in database) and (not item in filebase) and (not item in rssbase):
     raise Exception("Item " + item + " should not be in full list")
